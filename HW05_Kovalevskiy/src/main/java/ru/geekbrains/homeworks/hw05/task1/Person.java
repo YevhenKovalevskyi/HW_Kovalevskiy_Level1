9a3b9c951ae2d8a6fa95d53936be0bef7f71329b
e.kovalevskiy <carsar.sd@gmail.com>
2021-05-27 21:32:35 +0300

    refs #5 - Homework #5

---
+++ HW05_Kovalevskiy/src/main/java/ru/geekbrains/homeworks/hw05/task1/Person.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.homeworks.hw05.task1;
+
+import lombok.Value;
+
+@Value
+public class Person {
+    
+    String fio;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+}
